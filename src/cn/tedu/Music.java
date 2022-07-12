ee8172d71cdcaa762de2580831a8c4ab7347eb36
N1ghtss <635349188@qq.com>
2022-07-12 15:35:01 +0800

    Music

---
+++ src/cn/tedu/Music.java
@@ -0,0 +1,18 @@
+package cn.tedu;
+
+import javazoom.jl.player.Player;
+
+import java.io.FileInputStream;
+
+public class Music extends Thread{
+
+    public void run(){
+        try {
+            FileInputStream file=new FileInputStream("Music\\洋艺洋 - 就是爱你 (原唱_ 陶喆) (clip).mp3");
+            Player player=new Player(file);
+            player.play();
+        }catch (Exception e){
+            e.printStackTrace();
+                    }
+    }
+}
